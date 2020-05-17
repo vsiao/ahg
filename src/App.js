8b80f5ec60fc37d9dc9ab00de7f82c4fb4551ec5
Vincent Siao <vincent@asana.com>
2020-05-16 19:33:34 -0700

    Deploy with gh-pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <span className="App-ahgName">Armpittsburgh Hotel Group</span>
       </header>
       <div className="App-main">
-        <img className="App-heroImg" src="/house-of-bars.jpg" />
+        <img className="App-heroImg" src="./house-of-bars.jpg" />
         <div className="App-page">
           <div className="App-contents">
             <h1 className="App-title">Experience the House of Lars</h1>
